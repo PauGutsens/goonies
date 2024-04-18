0481062c7af867e5051d8fcf2d151538fda47722
PauGutsens <159762692+PauGutsens@users.noreply.github.com>
2024-04-18 18:13:16 +0200

    Enemy.h

---
+++ The goonies/src/Enemy.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "Entity.h"
+#include "TileMap.h"
+
+//Representation model size: 32x32
+#define ENEMY_FRAME_SIZE		32
+//Logical model size: 12x28
+#define ENEMY_PHYSICAL_WIDTH	12
+#define ENEMY_PHYSICAL_HEIGHT	2
+
+//Logic states
+enum class State { IDLE, WALKING, DEAD };
+enum class Look { RIGHT, LEFT };
+
+//Rendering states
+
+enum class PlayerAnim {
+
+	IDLE_LEFT, IDLE_RIGHT,
+	WALKING_LEFT, WALKING_RIGHT,
+	NUM_ANIMATIONS
+
+};
+
+
+
+class Enemy: public Entity
+
+{
+
+public:
+
+	Enemy(const Point& p, State s, Look view);
+	~Enemy();
+	AppStatus Initialise();
+	void SetTileMap(TileMap* tilemap);
+	void IncrScore(int n);
+	void Update();
+	void DrawDebug(const Color& col) const;
+	void Release();
+
+
+
+private:
+
+	bool IsLookingRight() const;
+
+	bool IsLookingLeft() const;
+
+
+
+	//Player mechanics
+
+	void MoveX();
+	void MoveY();
+
+	//Animation management
+	void SetAnimation(int id);
+
+	EnemyAnim GetAnimation();
+	void StartWalkingLeft();
+	void StartWalkingRight();
+	void ChangeAnimRight();
+	void ChangeAnimLeft();
+
+	State state;
+	TileMap *map;
+	int score;
+
+};
+

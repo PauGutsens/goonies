98eb1fedf53bfdcf02ff5af5119b42cd127648bc
PauGutsens <159762692+PauGutsens@users.noreply.github.com>
2024-04-19 09:10:00 +0200

    Enemy.cpp

--- The goonies/src/Enemy.cpp
+++ The goonies/src/Enemy.cpp
@@ -3,6 +3,7 @@
 #include "TileMap.h"
 #include "Globals.h"
 #include <raymath.h>
+#include <Player.h>
 Ennemy::Enemy(const Point& p, State s, Look view) :
 	Entity(p, ENEMY_PHYSICAL_WIDTH, ENEMY_PHYSICAL_HEIGHT, ENEMY_FRAME_SIZE, ENEMY_FRAME_SIZE)
 {

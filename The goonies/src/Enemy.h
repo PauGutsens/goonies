fbe2ab19df7cd18c25dad29e547f8976fad96c65
PauGutsens <159762692+PauGutsens@users.noreply.github.com>
2024-04-19 08:38:44 +0200

    Enemy.h

--- The goonies/src/Enemy.h
+++ The goonies/src/Enemy.h
@@ -3,9 +3,9 @@
 #include "TileMap.h"
 
 //Representation model size: 32x32
-#define ENEMY_FRAME_SIZE		32
+#define ENEMY_FRAME_SIZE		8
 //Logical model size: 12x28
-#define ENEMY_PHYSICAL_WIDTH	12
+#define ENEMY_PHYSICAL_WIDTH	6
 #define ENEMY_PHYSICAL_HEIGHT	2
 
 //Logic states
@@ -13,43 +13,28 @@ enum class State { IDLE, WALKING, DEAD };
 enum class Look { RIGHT, LEFT };
 
 //Rendering states
-
 enum class EnemyAnim {
 
 	IDLE_LEFT, IDLE_RIGHT,
 	WALKING_LEFT, WALKING_RIGHT,
 	NUM_ANIMATIONS
-
 };
-
-
-
 class Enemy: public Entity
-
 {
-
 public:
-
 	Enemy(const Point& p, State s, Look view);
 	~Enemy();
 	AppStatus Initialise();
 	void SetTileMap(TileMap* tilemap);
-	void IncrScore(int n);
+	void SetScore(int n);
 	void Update();
 	void DrawDebug(const Color& col) const;
 	void Release();
-
-
-
 private:
-
 	bool IsLookingRight() const;
-
 	bool IsLookingLeft() const;
 
-
-
-	//Player mechanics
+	//Enemy mechanics
 
 	void MoveX();
 	void MoveY();
@@ -58,8 +43,8 @@ private:
 	void SetAnimation(int id);
 
 	EnemyAnim GetAnimation();
-	void StartWalkingLeft();
-	void StartWalkingRight();
+	void WalkingLeft();
+	void WalkingRight();
 	void ChangeAnimRight();
 	void ChangeAnimLeft();
 
